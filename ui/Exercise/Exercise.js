ab2765dd9701554407ccb7c2db8f987d6d54bd21
Jonas Plein <jonas.plein@smail.inf.h-brs.de>
2021-10-11 20:40:50 +0200

    feat: make component for dynamically generating html code using template strings for the exercise structure

---
+++ ui/Exercise/Exercise.js
@@ -0,0 +1,61 @@
+const renderRequestReply = (requestText, replyText, requestClass, replyClass) =>
+    `
+    <div class="${requestClass}">${requestText}</div>
+    <div class="${replyClass}">${replyText}</div>
+    `
+
+const renderTask = (data, taskWrapperClass, titleClass, requestClass, replyClass) =>
+    `
+    <div class="${taskWrapperClass}">
+        <div class="${titleClass}">${data.title}</div>
+        ${data.text.map(item => renderRequestReply(item.request, item.reply, requestClass, replyClass)).join('')}
+    </div>
+    `
+
+const renderExercise = (
+    data,
+    exerciseWrapperClass,
+    exerciseClass,
+    taskWrapperClass,
+    titleClass,
+    requestClass,
+    replyClass
+) =>
+    `
+        <div class="${exerciseWrapperClass}">
+            <div class="${exerciseClass}">${data.title}</div>
+            ${data.tasks.map(task => renderTask(
+                task,
+                taskWrapperClass,
+                titleClass,
+                requestClass,
+                replyClass)).join('')}
+        </div>
+    `
+
+export const renderComponent = (
+    query,
+    givenId,
+    givenClass,
+    data,
+    exerciseWrapperClass,
+    exerciseClass,
+    taskWrapperClass,
+    titleClass,
+    requestClass,
+    replyClass
+) => {
+    const current = document.querySelector(`script#${query}`);
+    const replacement = document.createElement("div");
+    replacement.setAttribute("id", givenId);
+    replacement.setAttribute("class", givenClass);
+    replacement.innerHTML = renderExercise(
+        data,
+        exerciseWrapperClass,
+        exerciseClass,
+        taskWrapperClass,
+        titleClass,
+        requestClass,
+        replyClass);
+    current.parentNode.replaceChild(replacement, current);
+}
