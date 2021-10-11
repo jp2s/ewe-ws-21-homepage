d2a02019a6ee1f7de8223a30ddfa284a5defa713
Jonas Plein <jonas.plein@smail.inf.h-brs.de>
2021-10-11 20:39:15 +0200

    feat: outsource the Header html code and make it reusable using template strings

--- ui/Header/Header.js
+++ ui/Header/Header.js
@@ -1,3 +1,34 @@
-import {createComponent} from "../../util/util.js";
+const baseUrl = "http://www2.inf.h-brs.de/~jplein2s"
 
-createComponent("cHeader","ui/Header/Header.html", "header", "container");
+const goToPage = (page) => `{window.location.pathname = '${baseUrl}/pages/${page}.html'}`
+const goToHome = () => `{window.location.pathname = '${baseUrl}/index.html'}`
+
+const renderHeader = () =>
+    `
+    <div id="headerTitle" class="container" onclick="${goToHome()}">
+        <div class="boldText">2021 WS - Einführung in Web Engineering</div>
+        <div>Homepage</div>
+    </div>
+    <div id="navBar" class="container">
+        <div class="navBarItem" onclick="${goToPage('one')}">one</div>
+        <div class="navBarItem">two</div>
+        <div class="navBarItem">three</div>
+        <div class="navBarItem">four</div>
+        <div class="navBarItem">five</div>
+        <div class="navBarItem">six</div>
+        <div class="navBarItem">seven</div>
+        <div class="navBarItem">eight</div>
+        <div class="navBarItem">nine</div>
+        <div class="navBarItem">ten</div>
+        <div class="navBarItem">eleven</div>
+        <div class="navBarItem">twelve</div>
+    </div>
+    <hr align="left" id="headerDivider">
+    `
+
+const current = document.querySelector("script#cHeader");
+const replacement = document.createElement("div");
+replacement.setAttribute("id", "header");
+replacement.setAttribute("class", "container");
+replacement.innerHTML = renderHeader();
+current.parentNode.replaceChild(replacement, current);
