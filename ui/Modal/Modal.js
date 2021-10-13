bbd3b3db7d3eee942cf29429764c2d2d25866eb9
Jonas Plein <jonas.plein@smail.inf.h-brs.de>
2021-10-13 14:53:01 +0200

    feat: add class for the close button in the Modal component

--- ui/Modal/Modal.js
+++ ui/Modal/Modal.js
@@ -2,38 +2,27 @@ const toggleModal = (modalClass, show) =>
     show ? `document.getElementsByClassName('${modalClass}')[0].style.removeProperty('display');`
         : `document.getElementsByClassName('${modalClass}')[0].style.setProperty('display', 'none');`
 
-const renderCloseButton = (modalClass) =>
+const renderCloseButton = (modalClass, closeButtonClass) =>
     `
-    <div style="
-            cursor: pointer;
-            font-family: appleGaramondBoldItalic;
-            font-size: 30px;
-            text-align: center;
-            color: black;
-            border: 4px solid purple;
-            background: rgba(255, 255, 255, 0.8);
-            width: 30%;
-            padding-top: 4px;
-            padding-bottom: 4px;
-            "
-        onclick="${toggleModal(modalClass,false)}">close
+    <div class="${closeButtonClass}" onclick="${toggleModal(modalClass,false)}">
+        close
     </div>
     `
 
-const renderModal = (modalClass, modalWrapper, content) =>
+const renderModal = (modalClass, modalWrapper, closeButtonClass, content) =>
     `
     <div class="${modalClass}" style="display: none;">
         <div class="${modalWrapper}">
             ${content}
-            ${renderCloseButton(modalClass)}
+            ${renderCloseButton(modalClass, closeButtonClass)}
         </div>
     </div>
     `
 
-export const renderComponentModal = (modalClass, modalWrapper, buttonClass, content) =>
+export const renderComponentModal = (modalClass, modalWrapper, openButtonClass, closeButtonClass, content) =>
     `
-    <div class="${buttonClass}" onclick="${toggleModal(modalClass,true)}">
+    <div class="${openButtonClass}" onclick="${toggleModal(modalClass,true)}">
         click to expand
     </div>
-    ${renderModal(modalClass, modalWrapper, content)}
+    ${renderModal(modalClass, modalWrapper, closeButtonClass, content)}
     `
