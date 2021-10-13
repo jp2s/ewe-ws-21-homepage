39b5847ba9ecc68c5302200b5923ff87e607b1d6
Jonas Plein <jonas.plein@smail.inf.h-brs.de>
2021-10-13 12:55:33 +0200

    refactor: use the Modal component in Wireframe

--- ui/Wireframe/Wireframe.js
+++ ui/Wireframe/Wireframe.js
@@ -1,44 +1,16 @@
-const toggleModal = (modalClass, show) =>
-    show ? `document.getElementsByClassName('${modalClass}')[0].style.removeProperty('display');`
-        : `document.getElementsByClassName('${modalClass}')[0].style.setProperty('display', 'none');`
+import {renderComponentModal} from "../Modal/Modal.js";
 
-const renderModal = (modalClass, modalWrapper, imgSrc, imgClass,) =>
+const renderWireframeRequest = (requestText, requestClass) =>
     `
-        <div class="${modalClass}" style="display: none;">
-        <div class="${modalWrapper}">
-            <img src="${imgSrc}" class="${imgClass}">
-            <div style="
-                cursor: pointer;
-                font-family: appleGaramondBoldItalic;
-                font-size: 30px;
-                text-align: center;
-                color: black;
-                border: 4px solid purple;
-                background: rgba(255, 255, 255, 0.8);
-                width: 30%;
-                padding-top: 4px;
-                padding-bottom: 4px;
-            "
-            onclick="${toggleModal(modalClass,false)}"
-            >close</div>
-        </div>
-        </div>
+    <div class="${requestClass}">${requestText}</div>
     `
 
 const renderImagePreview = (imgSrc, imgPreviewWrapperClass, imgPreviewClass, buttonClass, modalClass, modalWrapper, imgClass) =>
     `
     <div class="${imgPreviewWrapperClass}">
         <img src=${imgSrc} class="${imgPreviewClass}">
-        <div class="${buttonClass}" onclick="${toggleModal(modalClass,true)}">
-            click to expand
-        </div>
-        ${renderModal(modalClass, modalWrapper, imgSrc, imgClass)}
+        ${renderComponentModal(modalClass, modalWrapper, buttonClass, `<img src="${imgSrc}" class="${imgClass}">`)}
     </div>
-    `
-
-const renderWireframeRequest = (requestText, requestClass) =>
-    `
-    <div class="${requestClass}">${requestText}</div>
     `
 
 export const renderWireframe = (
@@ -70,25 +42,25 @@ export const renderWireframe = (
     </div>
     `
 
-export const renderComponentWireframe = (
-    query,
-    givenId,
-    givenClass,
-    data,
-    wireframeWrapperClass,
-    requestClass,
-    imgPreviewWrapperClass,
-    imgPreviewClass,
-) => {
-    const current = document.querySelector(`script#${query}`);
-    const replacement = document.createElement("div");
-    replacement.setAttribute("id", givenId);
-    replacement.setAttribute("class", givenClass);
-    replacement.innerHTML = renderWireframe(
-        data,
-        wireframeWrapperClass,
-        requestClass,
-        imgPreviewWrapperClass,
-        imgPreviewClass);
-    current.parentNode.replaceChild(replacement, current)
-}
+// export const renderComponentWireframe = (
+//     query,
+//     givenId,
+//     givenClass,
+//     data,
+//     wireframeWrapperClass,
+//     requestClass,
+//     imgPreviewWrapperClass,
+//     imgPreviewClass,
+// ) => {
+//     const current = document.querySelector(`script#${query}`);
+//     const replacement = document.createElement("div");
+//     replacement.setAttribute("id", givenId);
+//     replacement.setAttribute("class", givenClass);
+//     replacement.innerHTML = renderWireframe(
+//         data,
+//         wireframeWrapperClass,
+//         requestClass,
+//         imgPreviewWrapperClass,
+//         imgPreviewClass);
+//     current.parentNode.replaceChild(replacement, current)
+// }
