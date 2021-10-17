7d7d9c9fab7bd8009c5f463a5ffb8ab45e2617ce
Jonas Plein <jonas.plein@smail.inf.h-brs.de>
2021-10-17 11:19:10 +0200

    feat: add exercise two component

---
+++ exercises/two.js
@@ -0,0 +1,15 @@
+import {exerciseTwo} from "../db/exerciseTwo.js";
+import {renderComponentExercise} from "../ui/Exercise/Exercise.js";
+
+renderComponentExercise(
+    "cTwo",
+    "exTwo",
+    "exerciseTwo",
+    exerciseTwo,
+    "twoExerciseWrapperClass",
+    "twoExerciseTitle",
+    "twoTaskWrapperClass",
+    "twoTaskTitle",
+    "twoTaskRequest",
+    "twoTaskReply"
+)
