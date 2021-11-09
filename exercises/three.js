7bebb06693b9e762cde318bb7db745f5bcd13be8
Jonas Plein <jonas.plein@smail.inf.h-brs.de>
2021-11-09 16:59:51 +0100

    feat: add all files for exercise three including the new table of content

---
+++ exercises/three.js
@@ -0,0 +1,25 @@
+import {exerciseThree, tocThree} from "../db/exerciseThree.js";
+import {renderComponentExercise} from "../ui/Exercise/Exercise.js";
+
+renderComponentExercise(
+    {
+        query: "cThree",
+        givenId: "exThree",
+        givenClass: "exerciseThree"
+    },
+    exerciseThree,
+    {
+        exerciseWrapperClass: "threeExerciseWrapperClass",
+        exerciseClass: "threeExerciseTitle",
+        taskWrapperClass: "threeTaskWrapperClass",
+        titleClass: "threeTaskTitle",
+        requestClass: "threeTaskRequest",
+        replyClass: "threeTaskReply",
+        tocWrapperClass: "threeTocWrapperClass",
+        tocListWrapper: "threeTocListWrapper",
+        tocDescription: "threeTocDescription",
+        tocContentWrapper: "threeTocContentWrapper",
+        tocTitle: "threeTocTitle",
+    },
+    tocThree,
+)
