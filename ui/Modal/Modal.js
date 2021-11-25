0cb208699aad0c6844936db5065cf5ce66061f2c
Jonas Plein <jonas.plein@smail.inf.h-brs.de>
2021-11-25 19:18:51 +0100

    refactor: pass additional modal iq to the modal rendering methods for easier toggle

--- ui/Modal/Modal.js
+++ ui/Modal/Modal.js
@@ -1,6 +1,6 @@
-const toggleModal = (givenModalClass, show) =>
-    show ? `document.getElementsByClassName('${givenModalClass}')[0].style.removeProperty('display');`
-        : `document.getElementsByClassName('${givenModalClass}')[0].style.setProperty('display', 'none');`
+const toggleModal = (fullModalId, show) =>
+    show ? `document.getElementsByClassName('${fullModalId}')[0].style.removeProperty('display');`
+        : `document.getElementsByClassName('${fullModalId}')[0].style.setProperty('display', 'none');`
 
 const renderCloseButton = (classes, givenModalClass) =>
     `
@@ -11,22 +11,22 @@ const renderCloseButton = (classes, givenModalClass) =>
     </div>
     `
 
-const renderModal = (classes, content, givenModalClass) =>
+const renderModal = (classes, content, givenModalClass, fullModalId) =>
     `
-    <div class="${givenModalClass}" style="display: none;">
+    <div class="${givenModalClass} ${fullModalId}" style="display: none;">
         <div class="${classes.modalWrapper}">
             ${content}
-            ${renderCloseButton(classes, givenModalClass)}
+            ${renderCloseButton(classes, fullModalId)}
         </div>
     </div>
     `
 
-export const renderComponentModal = (classes, content, givenModalClass) =>
+export const renderComponentModal = (classes, content, givenModalClass, fullModalId) =>
     `
     <div 
         class="${classes.openButtonClass}" 
-        onclick="${toggleModal(givenModalClass,true)}">
+        onclick="${toggleModal(fullModalId,true)}">
             click to expand
     </div>
-    ${renderModal(classes, content, givenModalClass)}
+    ${renderModal(classes, content, givenModalClass, fullModalId)}
     `
