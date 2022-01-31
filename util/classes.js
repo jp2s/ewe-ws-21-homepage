7a2ad559a63052b82133f6ca1e67e208f65419f2
Jonas Plein <jonas.plein@smail.inf.h-brs.de>
2022-01-31 19:56:43 +0100

    feat: add defaults for classes and styles

---
+++ util/classes.js
@@ -0,0 +1,43 @@
+export const defaultCodeClasses = {
+    codeTitleClass: "defaultCodeTitleClass",
+    codeWrapperClass: "defaultCodeWrapperClass"
+}
+
+export const defaultExerciseClasses = {
+    exerciseWrapperClass: "defaultExerciseWrapperClass",
+    exerciseClass: "defaultExerciseTitle",
+    taskWrapperClass: "defaultTaskWrapperClass",
+    titleClass: "defaultTaskTitle",
+    requestClass: "defaultTaskRequest",
+    replyClass: "defaultTaskReply",
+    tocWrapperClass: "defaultTocWrapperClass",
+    tocListWrapper: "defaultTocListWrapper",
+    tocDescription: "defaultTocDescription",
+    tocContentWrapper: "defaultTocContentWrapper",
+    tocTitle: "defaultTocTitle",
+    goToToWrapper: "defaultGoToToWrapper",
+    goToTopArrow: "defaultGoToTopArrow"
+}
+
+export const defaultWireframeClasses = {
+    wireframeWrapperClass: "defaultWireframeWrapperClass",
+    taskPageWrapperClass: "defaultWireframeWrapperClass",
+    requestClass: "defaultTaskRequest",
+    imgPreviewWrapperClass: "defaultImgPreviewWrapperClass",
+    imgPreviewClass: "defaultImgPreviewClass",
+    openButtonClass: "defaultOpenButtonClass",
+    closeButtonClass: "defaultCloseButtonClass",
+    modalClass: "defaultModalClass",
+    modalWrapper: "defaultModalWrapper",
+    imgClass: "defaultImgClass",
+    codeTitleClass: "defaultCodeTitleClass",
+    codeWrapperClass: "defaultCodeWrapperClass",
+    iframeTitleClass: "defaultIframeTitleClass",
+    wireframePagePreviewContainerClass: "defaultWireframePagePreviewContainerClass",
+    wireframePagePreviewWrapperClass: "defaultWireframePagePreviewWrapperClass",
+    wireframePagePreviewClass: "defaultWireframePagePreviewClass",
+    wireframePageModalClass: "defaultWireframePageModalClass",
+    wireframePageModalWrapper: "defaultWireframePageModalWrapper",
+    wireframePageClass: "defaultWireframePageClass",
+    wireframePageRefClass: "defaultWireframePageRefClass"
+}
