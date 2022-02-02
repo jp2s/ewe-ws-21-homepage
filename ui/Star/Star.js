7409f04f8d05efd8b7128dfe80aa4a160a8afa2b
Jonas Plein <jonas.plein@smail.inf.h-brs.de>
2022-02-02 14:12:00 +0100

    feat: add component for displaying an SVG star

---
+++ ui/Star/Star.js
@@ -0,0 +1,15 @@
+export const renderStar = () =>
+    `
+    <div class="star">
+        <svg width="60" height="60">
+            <path d="M 30 10 
+                L 35 25 L 50 25 
+                L 37 35 L 40 50 
+                L 30 42 L 20 50 
+                L 23 35 L 10 25 
+                L 25 25 z" 
+                stroke="gold" stroke-width="5" 
+            />
+        </svg>
+    </div>
+    `
