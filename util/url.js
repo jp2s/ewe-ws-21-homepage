400921bda2c63d0d641a97edb2432d29d6c24a60
Jonas Plein <jonas.plein@smail.inf.h-brs.de>
2022-08-17 21:48:32 +0200

    chore: change serverUrl to github pages

--- util/url.js
+++ util/url.js
@@ -1,6 +1,6 @@
 const isLocal = window.location.hostname === "localhost"
 
 const localUrl = "/ewe-2021-homepage"
-const serverUrl = "//www2.inf.h-brs.de/~jplein2s"
+const serverUrl = "https://jp2s.github.io/ewe-ws-21-homepage"
 
 export const baseUrl = isLocal ? localUrl : serverUrl
